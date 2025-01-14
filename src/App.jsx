33faef0ffc748f635512905553729fff3d2225ea
yeeun kim <kimyeeun0885@naver.com>
2025-01-14 16:18:16 +0900

    profile저장장

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/main" element={<Main />} />
-        <Route path="/login" element={<Login />} />
        </Routes>
   
     </Router>
