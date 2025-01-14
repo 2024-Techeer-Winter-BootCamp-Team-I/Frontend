756d9ec518ab3d64bffbb18d1551f542fee16a41
yeeun kim <kimyeeun0885@naver.com>
2025-01-15 03:11:48 +0900

    마이페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/main" element={<Main />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
