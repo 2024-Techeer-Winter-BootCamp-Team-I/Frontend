da6215d6f82ec08af6a67e7b984696feb41d7dac
daiseek <dnjsgml0578@gmail.com>
2025-01-15 02:21:01 +0900

    [design/#23]세팅페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import SettingPage from './pages/SettingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/main" element={<Main />} />
+        <Route path="/setting" element={<SettingPage />} />
       </Routes>
     </Router>
   );
