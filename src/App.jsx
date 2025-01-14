7c71963ee6864f3fdfed8614c484f86f35500c58
yeeun kim <kimyeeun0885@naver.com>
2025-01-15 01:57:26 +0900

    설계확인페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import SettingCheck from './pages/SettingCheck'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/main" element={<Main />} />
+        <Route path="/check" element={<SettingCheck/>} />
       </Routes>
     </Router>
   );
