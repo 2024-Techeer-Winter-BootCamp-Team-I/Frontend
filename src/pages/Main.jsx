aca022edb1d937d406b182134d33f2fa4f0f3e51
parkyugyeong <qkrb8019@gmai.com>
2025-01-15 03:35:24 +0900

    layout 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,10 @@
-import LoginModal from '../components/LoginMal';
+import Layout from './Layout';
 
 const Main = () => {
-  return <LoginModal></LoginModal>;
+  return (
+    <Layout>
+      <div className="text-white">안녕하세용</div>
+    </Layout>
+  );
 };
 export default Main;
