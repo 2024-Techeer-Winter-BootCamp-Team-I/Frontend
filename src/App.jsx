781335a2a602147affd4759fced348250056686b
parkyugyeong <qkrb8019@gmai.com>
2025-01-16 16:07:02 +0900

    스토리북설정, 온보딩

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SpecificPage from './pages/SpecificPage';
+import Onboarding from './pages/Onboarding';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/specific" element={<SpecificPage />} />
+        <Route path="/onboarding" element={<Onboarding />} />
       </Routes>
     </Router>
   );
