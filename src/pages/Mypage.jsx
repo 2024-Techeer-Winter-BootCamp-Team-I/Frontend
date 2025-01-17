0a07928904a03650f7cda1aecd339f75cefc26a5
yeeun kim <kimyeeun0885@naver.com>
2025-01-18 00:23:42 +0900

    마이페이지 퍼블리싱

---
+++ src/pages/Mypage.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout';
+import Myproject from "../components/MyProject";
+
+ 
+const MyPage = () => {
+    return (
+        <Layout>
+            <Myproject />
+        </Layout>
+    );
+};
+  
+export default MyPage;
