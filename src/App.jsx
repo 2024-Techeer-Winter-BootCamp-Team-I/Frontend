664bca8b0468f47d7163159219d6b365b1fda711
parkyugyeong <qkrb8019@gmai.com>
2025-01-17 21:53:02 +0900

    design: layout 구성완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SpecificPage from './pages/SpecificPage';
-import Onboarding from './pages/Onboarding';
-
-import Design from './pages/Design';
-import Api from './pages/Api';
-import Erd from './pages/Erd';
-import Diagram from './pages/Diagram';
-
-import InputPage from './pages/InputPage';
-
-
-import SettingPage from './pages/SettingPage';
-import FrontPackage from './pages/FrontPackage';
-import FrontBuild from './pages/FrontBuild';
-import FrontFramework from './pages/FrontFramework';
-import FrontLanguage from './pages/FrontLanguage';
-import BackFramework from './pages/BackFramework';
-import BackDatabase from './pages/BackDatabase';
+import Main from './pages/Main';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/specific" element={<SpecificPage />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/design" element={<Design />} />
-        <Route path="/api" element={<Api />} />
-        <Route path="/erd" element={<Erd />} />
-        <Route path="/diagram" element={<Diagram />} />
-        <Route path="/input" element={<InputPage />} />
-
-        <Route path="/settingpage" element={<SettingPage />} />
-        <Route path="/frontpackage" element={<FrontPackage />} />
-        <Route path="/frontbuild" element={<FrontBuild />} />
-        <Route path="/frontframework" element={<FrontFramework />} />
-        <Route path="/frontlanguage" element={<FrontLanguage />} />
-        <Route path="/backframework" element={<BackFramework />} />
-        <Route path="/backdatabase" element={<BackDatabase />} />
-
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Main.jsx
@@ -0,0 +1,6 @@
+import Layout from '../pages/Layout';
+
+const Main = () => {
+  return <Layout></Layout>;
+};
+export default Main;
