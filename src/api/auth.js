78d5a53bf8a1f7242f28931056e1c214845efe21
parkyugyeong <qkrb8019@gmai.com>
2025-01-22 15:48:11 +0900

    소셜로그인 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,10 @@
+// login 버튼을 클릭했을 때 호출되는 코드
 export function GithubLogin() {
   try {
+    // 서버 측 "/login" -> GitHub OAuth 화면으로 이동
     window.location.href = `${import.meta.env.VITE_API_BASE_URL}/login`;
-    console.log('click');
   } catch (error) {
-    console.log('깃허브 로그인 에러', error);
+    console.error('깃허브 로그인 에러', error);
     throw error;
   }
 }
