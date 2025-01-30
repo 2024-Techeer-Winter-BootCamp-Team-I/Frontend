505692c618a1040bf88caaf3fe143b2fcaa2452e
yeeun kim <kimyeeun0885@naver.com>
2025-01-31 02:38:49 +0900

    온보딩 수정 3

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -48,7 +48,7 @@ const OnboardingPage = () => {
 
   const splineScenes = [
     'https://prod.spline.design/sHgTaG64SnS6eQYL/scene.splinecode',
-    'https://prod.spline.design/3R0fvHMsohry1czX/scene.splinecode',
+    'https://prod.spline.design/yv9b6mRJsyD71IF5/scene.splinecode',
     'https://prod.spline.design/7IInnHw1sbK2Iaa8/scene.splinecode',
   ];
 
@@ -158,40 +158,42 @@ const OnboardingPage = () => {
         <section
           ref={(el) => (sectionsRef.current[1] = el)}
           className="flex h-screen w-full items-center justify-center relative z-10"
-          style={{
-            backgroundImage: `url(${gradationBackground})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            height: '100vh',
-          }}
         >
           <div ref={(el) => (splineRefs.current[1] = el)} className="absolute inset-0 z-0">
             <Spline scene={splineScenes[1]} />
           </div>
-          <div className="relative z-10 flex flex-col items-start w-[60%] ml-[1%]">
-            {/* 개발의 시작 박스 */}
-            <div className="bg-[#171717] bg-opacity-70 p-6 rounded-lg shadow-xl text-[#e57373] w-[22rem] h-[9rem] flex flex-col items-center justify-center text-center">
-              <h2 className="text-xl font-bold mb-4">개발의 시작, 어렵지 않나요?</h2>
+          <div className="relative w-full h-[40rem]">
+            {/* 개발의 시작 박스 1 (왼쪽에 꼬리) */}
+            <div className="relative top-[4rem] left-[22rem] bg-[#272727] bg-opacity-70 p-6 rounded-lg shadow-xl text-[#e57373] 
+       w-[18rem] h-[6rem] flex flex-col items-center justify-center text-center 
+       after:content-[''] after:absolute after:-bottom--1 after:left-[-1rem] 
+       after:border-8 after:border-transparent after:border-r-[#272727]">
               <ul className="space-y-1 text-[#e0e0e0] text-[0.9rem]">
                 <li>POC 설계, API & ERD 작성이 어려워요</li>
+              </ul>
+            </div>
+
+            {/* 개발의 시작 박스 2 (오른쪽에 꼬리) */}
+            <div className="relative top-[1rem] left-[45rem] bg-[#272727] bg-opacity-70 p-6 rounded-lg shadow-xl text-[#e57373] 
+       w-[18rem] h-[6rem] flex flex-col items-center justify-center text-center 
+       after:content-[''] after:absolute after:-bottom--1 after:right-[-1rem] 
+       after:border-8 after:border-transparent after:border-l-[#272727]">
+              <ul className="space-y-1 text-[#e0e0e0] text-[0.9rem]">
                 <li>FE/BE 환경세팅이 번거로워요</li>
               </ul>
             </div>
-
-            {/* 점선 화살표 (위치 조정) */}
-            <div className="w-[22rem] flex justify-center">
-              <div className="h-[36px] border-l-2 border-dashed border-white opacity-80"></div>
-            </div>
-
-            {/* Dev Sketch 해결 박스 */}
-            <div className="bg-[#051c2b] bg-opacity-70 p-6 rounded-lg shadow-xl text-[#64b5f6] w-[22rem] flex flex-col items-center justify-center text-center">
-              <h3 className="text-xl font-semibold mb-4">Dev Sketch가 해결합니다!</h3>
-              <ul className="space-y-1 text-[#e0e0e0] text-[0.9rem]">
-                <li>API & ERD & Diagram 생성</li>
-                <li>개발 초기환경 세팅</li>
-              </ul>
-            </div>
           </div>
+
+          <div className="absolute top-[22rem] left-[40rem] transform -translate-x-1/2 bg-[#051c2b] bg-opacity-70 
+  p-6 rounded-lg shadow-xl text-[#64b5f6] w-[22rem] h-[9rem] flex flex-col items-center justify-center text-center 
+  after:content-[''] after:absolute after:-right-4 after:top-1/2 after:transform after:-translate-y-1/2 
+  after:border-8 after:border-transparent after:border-l-[#193e56]">
+  <h3 className="text-xl font-semibold mb-4">Dev Sketch가 해결합니다!</h3>
+  <ul className="space-y-1 text-[#e0e0e0] text-[0.9rem]">
+    <li>API & ERD & Diagram 생성</li>
+    <li>개발 초기환경 세팅</li>
+  </ul>
+</div>
           <div className="absolute bottom-0 right-2 w-[10rem] h-16 bg-[#000000]"></div>
         </section>
 
@@ -199,18 +201,24 @@ const OnboardingPage = () => {
         <section
           ref={(el) => (sectionsRef.current[2] = el)}
           className="flex h-screen w-full items-center justify-center relative"
+          style={{
+            backgroundImage: `url(${gradationBackground})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+          }}
         >
           <div ref={(el) => (splineRefs.current[2] = el)} className="absolute inset-0 z-0">
             <Spline scene={splineScenes[2]} />
           </div>
           <div className="absolute z-10 top-[20rem] text-center">
             <div className="text-center ">
-              <h1 className="text-5xl font-bold text-[#a4d3f2]">Dev Sketch</h1>
+              <h1 className="text-5xl font-bold text-[#9ef3f3]">Dev Sketch</h1>
             </div>
             <div>
               <button
                 onClick={() => navigate('/')}
-                className="absolute w-[10rem] h-[2.5rem] top-[6rem] left-[3.4rem] start-button bg-[#132642]
+                className="absolute w-[10rem] h-[2.5rem] top-[6rem] left-[3.4rem] start-button bg-[#1b2d48]
                 bg-opacity-70 text-[rgb(220,220,220)] text-[1rem] font-medium px-6 py-2 rounded-lg"
               >
                 start now
