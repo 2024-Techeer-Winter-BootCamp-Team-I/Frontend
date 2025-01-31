9a577b761d71a40324c2e832139a58c5a5e2410e
parkyugyeong <qkrb8019@gmai.com>
2025-02-01 00:29:10 +0900

    modified:   src/pages/ErdPage.jsx

--- src/pages/ErdPage.jsx
+++ src/pages/ErdPage.jsx
@@ -129,7 +129,7 @@ const ErdPage = () => {
           </div>
 
           {/* ✨ ERD 크기 유지 + 중앙 정렬 */}
-          <div className="relative flex h-[600px] w-full max-w-4xl items-center justify-center rounded-lg border border-gray-600 bg-gray-800 p-2 shadow-lg">
+          <div className="relative flex h-[600px] w-full max-w-4xl items-center rounded-lg border border-gray-600 bg-gray-800 p-2 shadow-lg">
             <img
               src={SaveIcon}
               alt="Save"
